162478dc559eae7c8f165c357f3c3b05c88423bd
Ricardo Cardoso <rikardo_kz@live.com.pt>
2020-09-10 22:34:42 +0100

    Added functionality on the "about me" button

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// add a photo fade out 
+// add a photo fade out
+const aboutMeBtn = document.getElementById("btn2");
+const aboutEl = document.getElementById("hidden-about");
+
+// event listeners
+aboutMeBtn.addEventListener("click", () => {
+  aboutEl.style.display = "flex";
+});
+
+window.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("btn2")) {
+    aboutEl.style.display = "none";
+  }
+});
